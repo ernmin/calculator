2cc0baaee90db4b8cd210a29fcd9916859d0560d
ernmin <42770608+ernmin@users.noreply.github.com>
2024-07-02 00:00:04 +0800

    Add Event Listener to the Number Buttons AddEventListener to Number Buttons and the clear button Upon clicking the numbers, it will update the field

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-function add(first, second){
-    return Number(first) + Number(second);
-}
-
-function subtract(first, second){
-    return Number(first) - Number(second);
-}
-
-function multiply(first, second){
-    return Number(first) * Number(second);
-}
-
-function divide(first, second){
-    return Number(first) / Number(second);
-}
-
-function operate(operator, first, second){
-    if(operator === '+'){
-        add(first, second);
+document.addEventListener('DOMContentLoaded', function() {
+    function add(first, second){
+        return Number(first) + Number(second);
     }
-    else if(operator === '-'){
-        subtract(first, second);
+
+    function subtract(first, second){
+        return Number(first) - Number(second);
     }
-    else if(operator === '*'){
-        multiply(first, second);
+
+    function multiply(first, second){
+        return Number(first) * Number(second);
     }
-    else if(operator === '/'){
-        divide(first, second);
+
+    function divide(first, second){
+        return Number(first) / Number(second);
     }
-}
+
+    function operate(operator, first, second){
+        if(operator === '+'){
+            add(first, second);
+        }
+        else if(operator === '-'){
+            subtract(first, second);
+        }
+        else if(operator === '*'){
+            multiply(first, second);
+        }
+        else if(operator === '/'){
+            divide(first, second);
+        }
+    }
+
+    let numberButtons = document.querySelectorAll(".number");
+    let displayNumber = null;
+    for (let i = 0; i < numberButtons.length; i++){
+        numberButtons[i].addEventListener('click', function() {
+            text = i.toString();
+            if(displayNumber === null){
+                displayNumber = text;
+            }
+            else{
+                displayNumber += text;
+            }
+            let input = document.querySelector('.field');
+            input.textContent = displayNumber;
+        })
+    }
+
+    let clearField = document.querySelector('#AC');
+    clearField.addEventListener('click', function() {
+        let input = document.querySelector('.field');
+        displayNumber = null;
+        input.textContent = '';
+    })
+});
